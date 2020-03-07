28b8009c56f4fde4c21b4c529e3cd262a65f536c
Jawlit Patel <jawlit.patel@gmail.com>
2020-03-07 10:33:32 -0500

    debug

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,11 +2,5 @@
 
 def call(body) {
     echo "Start Deploy"
-
-    new Deployer(script:this).run()
-
     echo "Deployed"
-    currentBuild.result = 'SUCCESS' //FAILURE to fail
-
-    return this
 }
---
+++ vars/myDeliveryPipeline.groovy
@@ -0,0 +1,19 @@
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
+                }
+            }
+
+            stage('build') {
+                steps {
+                    sh 'mvn clean package -DskipTests=true'
+                }
+            }
+        }
+    }
+}
