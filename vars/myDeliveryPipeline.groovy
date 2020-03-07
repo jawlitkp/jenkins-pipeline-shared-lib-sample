c12cb28f0cf56eebd1de47852b01b23b95c780f5
Jawlit Patel <jawlit.patel@gmail.com>
2020-03-07 11:13:25 -0500

    debug

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -5,10 +5,37 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipelineParams
     body()
-
-    pipeline {
+    
+pipeline {
+podTemplate(yaml: """
+kind: Pod
+spec:
+  containers:
+  - name: maven
+    image: us.gcr.io/sharedinfra-svc-corp00/devopsjenkins/devops-jenkins-slave-maven:0.0.5
+    command:
+    - cat
+    tty: true
+  - name: kaniko
+    image: us.gcr.io/sharedinfra-svc-corp00/devopsjenkins/kaniko-executor:debug
+    imagePullPolicy: Always
+    command:
+    - /busybox/cat
+    tty: true
+    volumeMounts:
+      - name: jenkinsgcp
+        mountPath: /var/run/secrets/jenkinsgcp
+    env:
+      - name: GOOGLE_APPLICATION_CREDENTIALS
+        value: /var/run/secrets/jenkinsgcp/credentials.json
+  volumes:
+    - name: jenkinsgcp
+      secret:
+        secretName: jenkinsgcp
+"""
+  ) 
         stage('do some Docker work') {
 
         }
-    }
+}
 }
