75b8299f5d383b1eb5acfc3524965e33a5faa34d
Jawlit Patel <jawlit.patel@gmail.com>
2020-03-07 11:08:51 -0500

    debug

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -7,25 +7,8 @@ def call(body) {
     body()
 
     pipeline {
-        podTemplate(label: 'mypod', containers: [
-            containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat'),
-            containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.0', command: 'cat', ttyEnabled: true),
-            containerTemplate(name: 'helm', image: 'lachlanevenson/k8s-helm:latest', command: 'cat', ttyEnabled: true)
-        ],
-        volumes: [
-            hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
-        ]) 
-        node('mypod') {
-            stage('do some Docker work') {
-                container('docker') {                    
-                        sh """
-                            docker pull ubuntu
-                            docker tag ubuntu ${env.DOCKER_HUB_USER}/ubuntu:${env.BUILD_NUMBER}
-                            """
-                        sh "docker login -u ${env.DOCKER_HUB_USER} -p ${env.DOCKER_HUB_PASSWORD} "
-                        sh "docker push ${env.DOCKER_HUB_USER}/ubuntu:${env.BUILD_NUMBER} "
-                }
-            }
+        stage('do some Docker work') {
+
         }
     }
 }
